d6a74c34b04e84aab9f2c291c5aae2e962bdefc2
InkedWings <yezhijing110@outlook.com>
2025-11-03 18:57:11 +0000

    pass test

--- examples/resources/model/resnet.py
+++ examples/resources/model/resnet.py
@@ -90,23 +90,23 @@ class ResNet(nn.Module):
         super().__init__()
         self.in_planes = 64
         # For CIFAR10
-        # self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         # For Fashion-MNIST
         # self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
         # For Caltech101
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1)) # For Caltech101
+        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1)) # For Caltech101
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512 * block.expansion, num_classes)
 
         # For Caltech101
-        self.linear = nn.Linear(512 * block.expansion, 102)
+        # self.linear = nn.Linear(512 * block.expansion, 102)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -118,13 +118,13 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-        out = self.maxpool(out) # For Caltech101
+        # out = self.maxpool(out) # For Caltech101
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
-        out = self.avgpool(out)  # For Caltech101
+        # out = self.avgpool(out)  # For Caltech101
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
